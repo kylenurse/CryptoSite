a3170cdae0fdf610f6f517520b5a05d8f0556292
Kyle Nurse <kyle.nurse@torontomu.ca>
2024-03-31 23:47:53 -0400

    Boilerplate for cryptoSite

---
+++ CryptoSite.jsx
@@ -0,0 +1,10 @@
+export default function CryptoSite() {
+    return (
+        <>
+        <menu>
+            
+        </menu>
+        
+        </>
+    );
+}
---
+++ index.jsx
@@ -0,0 +1,7 @@
+import { ReactDOM } from "react";
+import CryptoSite from "./CryptoSite";
+import "./index.css";
+
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<CryptoSite />);
